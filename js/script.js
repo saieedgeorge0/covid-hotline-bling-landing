35347214165d658feccaa26058fc42c01f243f89
George Saieed <saieed.george0@gmail.com>
2020-03-28 18:37:01 -0400

    add some stuff

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+    $(".project-contain").hover(function() {
+        $(this).find(".project").css("padding", "30px 20px 30px");
+    }, function(){
+        $(this).find(".project").css("padding", "20px");
+    });
     $(".project-contain").click(function(){
         $(".project-contain").find(".expand-project").css("display", "none");
         $(".project-contain").find(".project").css("padding-top", "20px");
