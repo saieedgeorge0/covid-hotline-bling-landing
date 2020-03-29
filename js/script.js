2580eccd2f03864848e6fdde7940cb2b40912a54
George Saieed <saieed.george0@gmail.com>
2020-03-28 20:46:17 -0400

    finish most of site, begin mkaing responsive

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
-$(document).ready(function(){
-    $(".project-contain").hover(function() {
-        $(this).find(".project").css("padding", "30px 20px 30px");
-    }, function(){
-        $(this).find(".project").css("padding", "20px");
+$(document).ready(function() {
+    $(".adown1").click(function() {
+        $('html, body').animate({ scrollTop: $("#project1").offset().top }, 'slow');
     });
-    $(".project-contain").click(function(){
-        $(".project-contain").find(".expand-project").css("display", "none");
-        $(".project-contain").find(".project").css("padding-top", "20px");
-        $(".project-contain").find(".project").css("padding-bottom", "20px");
-        $(this).find(".expand-project").css("display", "block");
-        $(this).find(".image").css("box-shadow", "black 10px 10px 20px 1px");
-        $(this).find(".project").css("padding-top", "50px");
-        $(this).find(".project").css("padding-bottom", "30px");
+    $(".adown2").click(function() {
+        $('html, body').animate({ scrollTop: $("#project2").offset().top }, 'slow');
     });
-  });
+    $(".adown3").click(function() {
+        $('html, body').animate({ scrollTop: $("#project3").offset().top }, 'slow');
+    });
+
+    //     $(".project-contain").hover(function() {
+    //         $(this).find(".project").css("padding", "30px 20px 30px");
+    //     }, function(){
+    //         $(this).find(".project").css("padding", "20px");
+    //     });
+    //     $(".project-contain").click(function(){
+    //         $(".project-contain").find(".expand-project").css("display", "none");
+    //         $(".project-contain").find(".project").css("padding-top", "20px");
+    //         $(".project-contain").find(".project").css("padding-bottom", "20px");
+    //         $(this).find(".expand-project").css("display", "block");
+    //         $(this).find(".image").css("box-shadow", "black 10px 10px 20px 1px");
+    //         $(this).find(".project").css("padding-top", "50px");
+    //         $(this).find(".project").css("padding-bottom", "30px");
+    //     });
+});
